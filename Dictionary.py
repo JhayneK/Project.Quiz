bd20d03a2e93d25a39ee5a74fb6a7182f549a5bf
Jhayne Henemam <103227909+JhayneK@users.noreply.github.com>
2023-07-18 21:38:35 -0300

    Create Dictionary.py
    Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,12 @@
+questoes = {1: ["Qual é a capital da França?", "Paris", "Londres", "Roma", "Berlim", "A"],
+		2: ["Qual é a capital da Brasil?", "Santa Catarina", "Brasília", "Distrito Federal", "Acre", "B"],
+		3: ["Qual o resultado de 8 + 2?", "13", "14", "17", "10", "D"],
+		4: ["Quem pintou Mona Lisa?", "Picasso", "Da Vinci", "Van Gogh", "Warhol", "B"],
+		5: ["Qual é o maior planeta do sistema solar?", "Marte", "Saturno", "Jupiter", "Venus", "C"],
+		6: ["Quem foi o autor da obra Romeu e Julieta?", "William Shakespeare", "Charles Dickens",\
+      	"Mark Twain", "Jane Austen", "A"],
+		7: ["Quem foi o pintor do quadro A Ultima Ceia?", "Pablo Picasso", "Salvador Dali",\
+        "Michelangelo", "Leonardo Da Vinci", "D"],
+		8: ["Qual o simbolo quimico do ouro?", "Au", "Ag", "Cu", "Fe", "A"],
+		9: ["Qual é a Capital da Russia?", "São Petersburgo", "Kiev", "Varsóvia", "Moscou", "D"],
+		10: ["Qual a formula quimica da agua?", "H2O", "CO2", "NaCl", "CH4", "A"]
